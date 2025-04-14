63cb6834226091c5afca471ddd0f2dc40438f537
Shahriar Iravanian <siravan@svtsim.com>
2025-04-13 21:56:09 -0400

    Rational removed; comments

--- python/funcbuilder/__init__.py
+++ python/funcbuilder/__init__.py
@@ -3,47 +3,48 @@ import numbers
 
 from . import pyengine
 from . import builder
-   
+
 ############################################################################
-     
+
+
 class Func:
     def __init__(self, compiler):
         self.compiler = compiler
         self.count_states = self.compiler.count_states
         self.count_params = self.compiler.count_params
-        self.count_obs = self.compiler.count_obs        
+        self.count_obs = self.compiler.count_obs
 
     def __call__(self, *args):
         if len(args) > self.count_states:
-            p = np.array(args[self.count_states:], dtype="double")
+            p = np.array(args[self.count_states :], dtype="double")
             self.compiler.params[:] = p
-    
+
         if isinstance(args[0], numbers.Number):
-            u = np.array(args[:self.count_states], dtype="double")
+            u = np.array(args[: self.count_states], dtype="double")
             self.compiler.states[:] = u
             self.compiler.execute()
             return self.compiler.obs.copy()
         else:
             return self.call_vectorized(*args)
-            
+
     def call_vectorized(self, *args):
-        assert(len(args) >= self.count_states)
+        assert len(args) >= self.count_states
         shape = args[0].shape
         n = args[0].size
         h = max(self.count_states, self.count_obs)
         buf = np.zeros((h, n), dtype="double")
 
         for i in range(self.count_states):
-            assert(args[i].shape == shape)
-            buf[i,:] = args[i].ravel()            
-        
+            assert args[i].shape == shape
+            buf[i, :] = args[i].ravel()
+
         self.compiler.execute_vectorized(buf)
-        
+
         res = []
-        for i in range(self.count_obs):            
-            y = buf[i,:].reshape(shape)
+        for i in range(self.count_obs):
+            y = buf[i, :].reshape(shape)
             res.append(y)
-            
+
         return res
 
     def dump(self, name, what="scalar"):
@@ -64,16 +65,16 @@ class OdeFunc:
 
         self.compiler.execute(t)
         return self.compiler.diffs.copy()
-        
+
     def get_u0(self):
         return self.compiler.get_u0()
 
     def get_p(self):
         return self.compiler.get_p()
-        
+
     def dump(self, name, what="scalar"):
-        self.compiler.dump(name, what=what)        
-        
+        self.compiler.dump(name, what=what)
+
 
 class JacFunc:
     def __init__(self, compiler):
@@ -89,11 +90,11 @@ class JacFunc:
             self.compiler.params[:] = p
 
         self.compiler.execute()
-        jac = self.compiler.obs.copy()        
+        jac = self.compiler.obs.copy()
         return jac.reshape((self.count_states, self.count_states))
-        
+
     def dump(self, name, what="scalar"):
-        self.compiler.dump(name, what=what)        
+        self.compiler.dump(name, what=what)
 
 
 def compile_func(states, eqs, params=None, obs=None, ty="native", use_simd=True):
@@ -112,14 +113,16 @@ def compile_ode(iv, states, odes, params=None, ty="native", use_simd=False):
         return OdeFunc(compiler)
     else:
         raise ValueError("unsupported platform")
-    
+
+
 def compile_jac(iv, states, odes, params=None, ty="native", use_simd=False):
     if pyengine.can_compile():
         model = pyengine.tree.model_jac(iv, states, odes, params)
         compiler = pyengine.PyCompiler(model)
         return JacFunc(compiler)
     else:
-        raise ValueError("unsupported platform")        
+        raise ValueError("unsupported platform")
+
 
 def FuncBuilder(*states):
     if pyengine.can_compile():
@@ -127,4 +130,3 @@ def FuncBuilder(*states):
         return B, B.states
     else:
         raise ValueError("unsupported platform")
-
--- python/funcbuilder/builder.py
+++ python/funcbuilder/builder.py
@@ -5,72 +5,75 @@ from . import pyengine
 
 tree = pyengine.tree
 
+
 class Builder:
     def __init__(self, *states):
         self.states = [tree.Var(x) for x in states]
-        self.obs = []
-        self.eqs = []
-        self.hits = {}
-        
+        self.obs = []  # observables (intermediate variables)
+        self.eqs = []  # equations
+        self.hits = {}  # number of times each observable is used
+
     def new_var(self):
+        """Creates a new observable"""
         k = len(self.obs)
         v = tree.Var(f"${k}")
         self.obs.append(v)
         self.hits[v] = 0
         return v
-        
+
     def prep(self, a):
+        """Called on each observable on the right hand side of an equation"""
         if isinstance(a, numbers.Number):
-            return tree.Const(a)            
-        if a in self.obs:            
-            self.hits[a] += 1            
-        return a                    
-        
+            return tree.Const(a)
+        if a in self.obs:
+            self.hits[a] += 1
+        return a
+
     def append_unary(self, op, a):
-        a = self.prep(a)        
+        a = self.prep(a)
         v = self.new_var()
         self.eqs.append(tree.Eq(v, tree.Unary(op, a)))
-        return v        
-        
+        return v
+
     def append_binary(self, op, a, b):
         a = self.prep(a)
-        b = self.prep(b)        
+        b = self.prep(b)
         v = self.new_var()
         self.eqs.append(tree.Eq(v, tree.Binary(op, a, b)))
         return v
-        
+
     def append_ifelse(self, cond, a, b):
         cond = self.prep(cond)
         a = self.prep(a)
         b = self.prep(b)
         v = self.new_var()
         self.eqs.append(tree.Eq(v, tree.IfElse(cond, a, b)))
-        return v        
-        
+        return v
+
     def add(self, *a):
-        if len(a) == 1:            
+        if len(a) == 1:
             return self.prep(a[0])
         elif len(a) > 2:
             t = self.add(*a[1:])
             return self.append_binary("plus", a[0], t)
-        else:            
+        else:
             return self.append_binary("plus", a[0], a[1])
-    
+
     def sub(self, a, b):
-        return self.append_binary("minus", a, b)        
-        
+        return self.append_binary("minus", a, b)
+
     def mul(self, *a):
         if len(a) == 1:
             return self.prep(a[0])
         elif len(a) > 2:
             t = self.add(*a[1:])
             return self.append_binary("times", a[0], t)
-        else:            
+        else:
             return self.append_binary("times", a[0], a[1])
-        
+
     def div(self, a, b):
         return self.append_binary("divide", a, b)
-        
+
     def pow(self, a, power):
         if power == 2:
             return self.append_unary("square", a)
@@ -84,119 +87,123 @@ class Builder:
         elif power == -3:
             t = self.append_unary("recip", a)
             return self.append_unary("cube", t)
-        elif power == Rational(1, 2):
+        elif power == 0.5:
             return self.append_unary("root", a)
-        elif power == Rational(3, 2):
+        elif power == 1.5:
             t = self.append_unary("root", a)
             return self.append_unary("cube", t)
         else:
             return self.append_binary("power", a, b)
-            
+
     def exp(self, a):
         return self.append_unary("exp", a)
-        
+
     def log(self, a):
         return self.append_unary("log", a)
-        
+
     def sqrt(self, a):
         return self.append_unary("root", a)
-        
+
     def square(self, a):
         return self.append_unary("square", a)
-        
+
     def cube(self, a):
         return self.append_unary("cube", a)
-        
+
     def recip(self, a):
         return self.append_unary("recip", a)
-            
+
     def sin(self, a):
         return self.append_unary("sin", a)
-        
+
     def cos(self, a):
         return self.append_unary("cos", a)
-        
+
     def tan(self, a):
         return self.append_unary("tan", a)
-        
+
     def sinh(self, a):
         return self.append_unary("sinh", a)
-        
+
     def cosh(self, a):
         return self.append_unary("cosh", a)
-        
+
     def tanh(self, a):
         return self.append_unary("tanh", a)
-        
+
     def asin(self, a):
         return self.append_unary("arcsin", a)
-        
+
     def acos(self, a):
         return self.append_unary("arccos", a)
-        
+
     def atan(self, a):
-        return self.append_unary("arctan", a)        
-        
+        return self.append_unary("arctan", a)
+
     def asinh(self, a):
         return self.append_unary("arcsinh", a)
-        
+
     def acosh(self, a):
         return self.append_unary("arccosh", a)
-        
+
     def atanh(self, a):
         return self.append_unary("arctanh", a)
-            
+
     def lt(self, a, b):
-        return self.append_binary("lt", a, b)                    
-        
+        return self.append_binary("lt", a, b)
+
     def leq(self, a, b):
-        return self.append_binary("leq", a, b)        
-        
+        return self.append_binary("leq", a, b)
+
     def gt(self, a, b):
-        return self.append_binary("gt", a, b)                    
-        
+        return self.append_binary("gt", a, b)
+
     def geq(self, a, b):
-        return self.append_binary("geq", a, b)                
-        
+        return self.append_binary("geq", a, b)
+
     def eq(self, a, b):
-        return self.append_binary("eq", a, b)        
-        
+        return self.append_binary("eq", a, b)
+
     def neq(self, a, b):
-        return self.append_binary("neq", a, b)        
-        
+        return self.append_binary("neq", a, b)
+
     def logical_and(self, a, b):
         return self.append_binary("and", a, b)
-        
+
     def logical_or(self, a, b):
         return self.append_binary("or", a, b)
-        
+
     def logical_xor(self, a, b):
-        return self.append_binary("xor", a, b)                        
-        
+        return self.append_binary("xor", a, b)
+
     def ifelse(self, cond, a, b):
-        return self.append_ifelse(cond, a, b)                    
-        
+        return self.append_ifelse(cond, a, b)
+
     def compile(self, y):
         try:
             eqs = self.arborize(y)
-            
+
             model = tree.Model(
-                tree.Var("$_"), # iv
-                self.states,    # states
-                [],             # params
-                self.obs,       # obs
-                eqs,            # eqs
-                [],             # odes
+                tree.Var("$_"),  # iv
+                self.states,  # states
+                [],  # params
+                self.obs,  # obs
+                eqs,  # eqs
+                [],  # odes
             )
-            
+
             idx = self.obs.index(y)
             compiler = pyengine.PyCompiler(model, ty="native")
-            return BuiltFunc(compiler, idx)  
+            return BuiltFunc(compiler, idx)
         except:
             raise ValueError(f"return variable {y} not found")
-            
-    def leaf(self, eqs, y, v):
-        try:        
+
+    def merge(self, eqs, y, v):
+        """Moves an observable v and merges it into the
+        destination tree if it is used only once.
+        y is the output observable and cannot be moved.
+        """
+        try:
             if v != y and self.hits[v] == 1:
                 idx = self.obs.index(v)
                 u = eqs[idx].rhs
@@ -204,70 +211,85 @@ class Builder:
                 return u
         except:
             return v
-        return v                            
+        return v
 
     def arborize(self, y):
+        """Merges expression trees if possible"""
         eqs = []
-        for i, eq in enumerate(self.eqs):                        
+
+        for i, eq in enumerate(self.eqs):
             if isinstance(eq.rhs, tree.Unary):
-                eqs.append(tree.Eq(eq.lhs, tree.Unary(
-                    eq.rhs.op, 
-                    self.leaf(eqs, y, eq.rhs.arg)
-            )))
+                eqs.append(
+                    tree.Eq(
+                        eq.lhs, tree.Unary(eq.rhs.op, self.merge(eqs, y, eq.rhs.arg))
+                    )
+                )
             elif isinstance(eq.rhs, tree.Binary):
-                eqs.append(tree.Eq(eq.lhs, tree.Binary(
-                    eq.rhs.op, 
-                    self.leaf(eqs, y, eq.rhs.left),
-                    self.leaf(eqs, y, eq.rhs.right),
-                )))
+                eqs.append(
+                    tree.Eq(
+                        eq.lhs,
+                        tree.Binary(
+                            eq.rhs.op,
+                            self.merge(eqs, y, eq.rhs.left),
+                            self.merge(eqs, y, eq.rhs.right),
+                        ),
+                    )
+                )
             elif isinstance(eq.rhs, tree.IfElse):
-                eqs.append(tree.Eq(eq.lhs, tree.IfElse(                        
-                    self.leaf(eqs, y, eq.rhs.cond),
-                    self.leaf(eqs, y, eq.rhs.true_val),
-                    self.leaf(eqs, y, eq.rhs.false_val)
-            )))                    
+                eqs.append(
+                    tree.Eq(
+                        eq.lhs,
+                        tree.IfElse(
+                            self.merge(eqs, y, eq.rhs.cond),
+                            self.merge(eqs, y, eq.rhs.true_val),
+                            self.merge(eqs, y, eq.rhs.false_val),
+                        ),
+                    )
+                )
             else:
-                eqs.append(tree.Eq(eq.lhs, eq.rhs))         
+                eqs.append(tree.Eq(eq.lhs, eq.rhs))
+        # the equations of the observables that are merged
+        # is set to None in the merge function and
+        # are removed here
         return [eq for eq in eqs if eq is not None]
-        
-        
+
+
 class BuiltFunc:
     def __init__(self, compiler, idx):
         self.compiler = compiler
         self.count_states = self.compiler.count_states
         self.count_params = self.compiler.count_params
-        self.count_obs = self.compiler.count_obs        
+        self.count_obs = self.compiler.count_obs
         self.idx = idx
 
     def __call__(self, *args):
         if len(args) > self.count_states:
-            p = np.array(args[self.count_states:], dtype="double")
+            p = np.array(args[self.count_states :], dtype="double")
             self.compiler.params[:] = p
-    
+
         if isinstance(args[0], numbers.Number):
-            u = np.array(args[:self.count_states], dtype="double")
+            u = np.array(args[: self.count_states], dtype="double")
             self.compiler.states[:] = u
             self.compiler.execute()
             return float(self.compiler.obs[self.idx])
         else:
             return self.call_vectorized(*args)
-            
+
     def call_vectorized(self, *args):
-        assert(len(args) >= self.count_states)
+        assert len(args) >= self.count_states
         shape = args[0].shape
         n = args[0].size
         h = max(self.count_states, self.count_obs)
         buf = np.zeros((h, n), dtype="double")
 
         for i in range(self.count_states):
-            assert(args[i].shape == shape)
-            buf[i,:] = args[i].ravel()            
-        
+            assert args[i].shape == shape
+            buf[i, :] = args[i].ravel()
+
         self.compiler.execute_vectorized(buf)
-        
-        res = buf[self.idx,:].reshape(shape)
+
+        res = buf[self.idx, :].reshape(shape)
         return res
 
     def dump(self, name, what="scalar"):
         self.compiler.dump(name, what=what)
-        
